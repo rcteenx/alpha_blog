5b285d645e9d2cb4c210d8600c88fed6f6bbbfc2
rcteenx <90903952+rcteenx@users.noreply.github.com>
2021-11-19 17:57:23 +0300

    Article index is ready.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,12 @@
 class ArticlesController < ApplicationController
 
   def show
-    #byebug
+    #byebug # debugs project...
     @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
   end
 
 end
