fd84f46ed1b6911788b556fb21f6c87da2e8bb48
rcteenx <90903952+rcteenx@users.noreply.github.com>
2021-11-20 23:12:43 +0300

    DRY Codes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,10 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def show
     #byebug # debugs project...
-    @article = Article.find(params[:id])
+    # set_article @article = Article.find(params[:id])
   end
 
   def index
@@ -14,12 +16,12 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+    # set_article @article = Article.find(params[:id])
   end
 
   def create
     # white list variables.
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     
     if @article.save
       flash[:notice] = "Article was created successfully."
@@ -32,16 +34,31 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    # set_article @article = Article.find(params[:id])
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully."
       redirect_to @article
     else 
       render 'edit'
     end
-    
-    
   end
 
+  def destroy
+    # set_article @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  # Cannot be used from outside
+  private
+  # anthing below this is private.
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   #root 'application#hello'
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  # if we expose all CRUDS - we do not need only! REST-ful routes
+  resources :articles
 end
